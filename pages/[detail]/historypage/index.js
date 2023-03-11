cdaf22e19fb731e45f6b0a77714d72eab49334b5
Swetha275 <swethikiswe@gmail.com>
2023-03-11 12:51:16 +0530

    100vh

--- pages/[detail]/historypage/index.js
+++ pages/[detail]/historypage/index.js
@@ -1,21 +1,35 @@
-import React, { Fragment } from 'react';
-import Navbar from '../../../Component/Navbar';
-import Head from 'next/head';
-
+import React, { useState, Fragment } from "react";
+import Navbar from "../../../Component/Navbar";
+import Head from "next/head";
 
 const History = ({ data }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm);
   return (
     <Fragment>
       <Head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"></link>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+        ></link>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <title>Visitors MS</title>
       </Head>
 
       <Navbar />
-      <div className="container-fluid">
+      <div className="headele">
         <h1 className="text-center p-3 fst-italic">Visitor's History</h1>
-        <div style={{ overflowX: 'auto' }}>
+      </div>
+      <div className="searchbar">
+        <img src="https://img.icons8.com/ios-glyphs/30/000000/search-client.png" />
+        <input
+          type="text"
+          placeholder="Search visitor..."
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </div>
+      <div className="container-fluid">
+        <div style={{ overflowX: "auto" }}>
           <table className="table table-striped ">
             <thead>
               <tr>
@@ -31,34 +45,63 @@ const History = ({ data }) => {
               </tr>
             </thead>
             <tbody>
-              {data.map(({ registerName, registerPurpose, registerReferrel, phoneNumber, address, in_time, out_time }, index) => {
-                return (
-                  <tr key={index}>
-                    <td style={{ fontSize: 15 }}>{registerName}</td>
-                    <td style={{ fontSize: 15 }}>{registerReferrel}</td>
-                    <td style={{ fontSize: 15 }}>{registerPurpose}</td>
-                    <td style={{ fontSize: 15 }}>{phoneNumber}</td>
-                    <td style={{ fontSize: 15 }}>{address}</td>
-                    {in_time
-                      .slice(0, 19)
-                      .split('T')
-                      .map(ele => {
-                        return <td style={{ fontSize: 15 }}>{ele}</td>;
-                      })}
+              {data
+                .filter((val) => {
+                  if (searchTerm == "") {
+                    return val;
+                  } else if (
+                    val.registerName
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                  ) {
+                    return val;
+                  }
+                })
+                .map(
+                  (
+                    {
+                      registerName,
+                      registerPurpose,
+                      registerReferrel,
+                      phoneNumber,
+                      address,
+                      in_time,
+                      out_time,
+                    },
+                    index
+                  ) => {
+                    return (
+                      <tr key={index}>
+                        <td style={{ fontSize: 15 }}>{registerName}</td>
+                        <td style={{ fontSize: 15 }}>{registerReferrel}</td>
+                        <td style={{ fontSize: 15 }}>{registerPurpose}</td>
+                        <td style={{ fontSize: 15 }}>{phoneNumber}</td>
+                        <td style={{ fontSize: 15 }}>{address}</td>
+                        {in_time
+                          .slice(0, 19)
+                          .split("T")
+                          .map((ele) => {
+                            return <td style={{ fontSize: 15 }}>{ele}</td>;
+                          })}
 
-                    {out_time
-                      .slice(0, 19)
-                      .split('T')
-                      .map(ele => {
-                        return <td style={{ fontSize: 15 }}>{ele}</td>;
-                      })}
-                  </tr>
-                );
-              })}
+                        {out_time
+                          .slice(0, 19)
+                          .split("T")
+                          .map((ele) => {
+                            return <td style={{ fontSize: 15 }}>{ele}</td>;
+                          })}
+                      </tr>
+                    );
+                  }
+                )}
             </tbody>
           </table>
         </div>
-        {data.length == 0 ? <p className="nodata">No visitors so far</p> : <p></p>}
+        {data.length == 0 ? (
+          <p className="nodata">No visitors so far</p>
+        ) : (
+          <p></p>
+        )}
         <br></br>
       </div>
     </Fragment>
@@ -69,7 +112,9 @@ export default History;
 
 export async function getServerSideProps(context) {
   const email = context.query.detail;
-  const res = await fetch(`https://visitors-vue-backend.onrender.com/${email}/show_users`);
+  const res = await fetch(
+    `https://visitors-vue-backend.onrender.com/${email}/show_users`
+  );
   const data = await res.json();
   return { props: { data } };
 }
